91329d886be23505110515b5f4505c44c68d281c
Minwoo Jo <minwoo0jo@hotmail.com>
2019-04-11 19:48:22 -0500

    make sure image resizing doesn't resize card

--- src/js/User/User.js
+++ src/js/User/User.js
@@ -159,8 +159,8 @@ class User extends Component {
         <div>
           <div className="Wrapper">
             <div className="UserSideBar">
-              <div className="card img-aspect">
-                <img className="card-img-top" src={'https://a.ppy.sh/' + this.state.pageData.user.userId + '_1552467424.jpeg'} alt="Profile"/>
+              <div className="card">
+                <img className="card-img-top img-aspect" src={'https://a.ppy.sh/' + this.state.pageData.user.userId + '_1552467424.jpeg'} alt="Profile"/>
                 <div className="card-body">
                   <h4 onMouseOver ={this.handleMouseIn} onMouseOut={this.handleMouseOut} className="card-text">{name}</h4>
                 </div>
