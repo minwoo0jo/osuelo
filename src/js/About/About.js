61e6d095dfbcf9d1916aa956af486418138255d7
Minwoo Jo <minwoo0jo@hotmail.com>
2019-05-19 13:56:15 -0500

    Update about page with correct calculations

--- src/js/About/About.js
+++ src/js/About/About.js
@@ -32,10 +32,11 @@ class About extends Component {
                             <p>
                                 The formula is shown here:<br/>
                                 <img src="https://wikimedia.org/api/rest_v1/media/math/render/svg/09a11111b433582eccbb22c740486264549d1129" alt="Elo Rating Calculation"/>
-                                <br/> where K is 90 until 10 matches played, then 60 until 30 matches played, then 30 afterwards. E is calculated with this formula: <br/>
+                                <br/> where K determined by the number of matches played and the type of tournament. K starts out at 90, then goes to 60 at 10 matches, then 30 at 30 matches.
+                                Only matches in international tournaments or matches against placed opponents count toward this number. E is calculated with this formula: <br/>
                                 <img src="https://wikimedia.org/api/rest_v1/media/math/render/svg/51346e1c65f857c0025647173ae48ddac904adcb" alt="Expected Elo Rating Calculation"/>
                                 <br/> The specifics of the math involved can be found in the <a href="https://en.wikipedia.org/wiki/Elo_rating_system">wikipedia page.</a>
-                                The only adjustment I've made to the formula is to reduce the K value to 1/3 when playing against an unplaced opponent (less than 10 matches played).
+                                The only adjustment I've made to the formula is to reduce the K value to 1/3 (or 1/6 if regional tournament) when playing against an unplaced opponent.
                                 <br/> It should be less punishing to lose against a top player who simply plays very few 1v1 tournaments in favor of team tournaments, and
                                 beating new players who might deserve a much lower elo than the starting elo should not overinflate the elo ratings of the people who beat them.
                             </p>
@@ -45,7 +46,7 @@ class About extends Component {
                             <p>
                                 Obviously, the system isn't without its flaws, and a major flaw is that until a decent number of matches are played, a player will most likely
                                 have an incorrect rating. Most of the cases of overinflated elo ratings can be found with players who regularly play matches against these players
-                                who have not found their true ratings yet.
+                                who have not found their true ratings yet. A placement system has been implemented to mitigate this problem, but it can still exist.
                             </p>
                             <br/><br/>
                         </div>
