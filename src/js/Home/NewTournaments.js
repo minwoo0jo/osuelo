1c9e62b3ad102afad6b02d8c96091564781800ac
Minwoo Jo <minwoo0jo@hotmail.com>
2020-12-14 21:04:46 -0600

    Use API for recent tournament data

--- src/js/Home/NewTournaments.js
+++ src/js/Home/NewTournaments.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import '../../resources/css/Home2.css';
 import { Link } from 'react-router-dom';
+import url from '../../resources/config.json';
+import axios from 'axios';
+import NotFound from '../NotFound.js';
+
 
 class NewTournaments extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            pageData: undefined,
+        }
+    }
+    componentDidMount() {
+        var endpoint = url.api + 'tournaments/'
+        axios.get(endpoint).then((response) => {
+          if(response.data.length === 0)
+            this.setState({pageData: null})
+          else
+            this.setState({pageData: response.data[1]})
+        }).catch((error) => {
+          console.log(error)
+          this.setState({pageData: null})
+        })
+    }
     render() {
         var imgChallonge = undefined
         try {
@@ -16,57 +38,73 @@ class NewTournaments extends Component {
         } catch {
             imgOsu = ''
         }
-        return (
-            <>
-                <h6>Newly Added Tournaments</h6>
+        if(this.state.pageData === null)
+            return (
+                <NotFound/>
+            )
+        var recentTournaments = [undefined, undefined]
+        if(this.state.pageData !== undefined)
+            for(var i = 0; i < 2; i++) {
+                var region = "- " + this.state.pageData[i].regionType
+                if(this.state.pageData[i].region === "None")
+                    region += ""
+                else
+                    region += " ("+this.state.pageData[i].region+")"
+                var rankRestrict = "- "
+                if(this.state.pageData[i].rankRestrict === "None" || (this.state.pageData[i].rankRestrict[0] === "1" && this.state.pageData[i].rankRestrict[1] === "-"))
+                    rankRestrict += "Open Rank"
+                else
+                    rankRestrict += this.state.pageData[i].rankRestrict + " Rank Range"
+                recentTournaments[i] =
                 <div className="card">
                     <div className="card-body" style={{textAlign: "left", paddingBottom: "0px"}}>
                         <div className="card-title" style={{float: "left"}}>
-                            <h4><Link to={{pathname: `/tournaments/id/124200`}}>osu! Finnish Tournament 4</Link></h4>
-                            <p>Added November 13, 2020</p>
+                            <h4><Link to={{pathname: `/tournaments/id/` + this.state.pageData[i].tournamentId}}>{this.state.pageData[i].tournamentName}</Link></h4>
+                            <p>Added {this.state.pageData[i].dateAdded}</p>
                         </div>
-                        <h6 className="card-subtitle mb-2 text-muted" style={{float: "right", paddingRight: "20px"}}>- Regional (Finland) <br/> - Open Rank</h6>
+                    <h6 className="card-subtitle mb-2 text-muted" style={{float: "right", paddingRight: "20px"}}>{region}<br/>{rankRestrict}</h6>
                     </div>
                     <div>
                         <div className="LeftSide" style={{paddingTop: "0px"}}>
                             <div className="card-body" style={{textAlign: "left"}}>
-                                <img className="card-img" src={require('../../resources/images/banners/OFT4.png')} alt="banner" />
+                                <img className="card-img" src={require('../../resources/images/banners/'+this.state.pageData[i].shortName+'.png')} alt="banner" />
                             </div>
                         </div>
                         <div className="RightSide" style={{paddingTop: "0px"}}>
                             <div className="card-body links">
                                 <h6 className="card-text">External Links</h6>
-                                <a href={"https://osu.ppy.sh/community/forums/topics/1120853"} target="_blank" rel="noreferrer noopener"><img width="50" height="50" src={imgOsu} alt="Challonge" /></a>
+                                <a href={this.state.pageData[i].forum} target="_blank" rel="noreferrer noopener"><img width="50" height="50" src={imgOsu} alt="Forum" /></a>
                                 <br/>
-                                <a href={"https://challonge.com/oft4"} target="_blank" rel="noreferrer noopener"><img width="50" height="50" src={imgChallonge} alt="Challonge" /></a>
+                                <a href={this.state.pageData[i].challonge} target="_blank" rel="noreferrer noopener"><img width="50" height="50" src={imgChallonge} alt="Challonge" /></a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="card">
+            }
+        else {
+            recentTournaments = 
+            <div className="card">
                     <div className="card-body" style={{textAlign: "left", paddingBottom: "0px"}}>
                         <div className="card-title" style={{float: "left"}}>
-                            <h4><Link to={{pathname: `/tournaments/id/124136`}}>osu! French Tournament 2020</Link></h4>
-                            <p>Added November 13, 2020</p>
+                            <h4>Loading...</h4>
                         </div>
-                        <h6 className="card-subtitle mb-2 text-muted" style={{float: "right", paddingRight: "20px"}}>- Regional (France) <br/> - Open Rank</h6>
                     </div>
                     <div>
                         <div className="LeftSide" style={{paddingTop: "0px"}}>
                             <div className="card-body" style={{textAlign: "left"}}>
-                                <img className="card-img" src={require('../../resources/images/banners/OFT2020.png')} alt="banner" />
                             </div>
                         </div>
                         <div className="RightSide" style={{paddingTop: "0px"}}>
-                            <div className="card-body">
-                                <h6 className="card-text">External Links</h6>
-                                <a href={"https://osu.ppy.sh/community/forums/topics/1114561"} target="_blank" rel="noreferrer noopener"><img width="50" height="50" src={imgOsu} alt="Challonge" /></a>
-                                <br/>
-                                <a href={"https://challonge.com/oft2020"} target="_blank" rel="noreferrer noopener"><img width="50" height="50" src={imgChallonge} alt="Challonge" /></a>
+                            <div className="card-body links">
                             </div>
                         </div>
                     </div>
                 </div>
+        }
+        return (
+            <>
+                <h6>Newly Added Tournaments</h6>
+                {recentTournaments}
                 <div className="card">
                     <div className="card-body">
                         <h5><Link to={{pathname: `/tournaments`}}>See the full tournament list</Link></h5>
